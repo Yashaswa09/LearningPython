9ba2d81ca21d59253a263cf40bb0ed5d221a2f87
Yashaswa Alguje <yashaswaalguje07@gmail.com>
2025-09-07 21:36:35 +0530

    Add files via upload

---
+++ 5.Dictianory.py
@@ -0,0 +1,19 @@
+marks = {
+    "Yashaswa": 100,
+    "kunal": 95,
+    "Jevansh": 88,
+    "Harsh": 85
+}
+# In Dict {} are used to make Dictianory 
+
+
+
+print(marks,type(marks))
+# print(marks.items())
+# print(marks.key())
+print(marks.values())
+marks.update({"Yashaswa":105,"Kelo":55})
+print(marks)
+
+print(marks.get("Yashaswa"))
+print(marks)
